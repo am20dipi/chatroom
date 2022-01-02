1b420b1a150b010af55591890bd0301e7d3f9290
Adriana <am20dipi@alum.siena.edu>
2022-01-02 15:12:20 -0500

    Installing np firebase; connecting to firebase backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+//console.log("hello")
+
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAu6iwGs95JYD9xTt1ZJJ5HIbw-P9LLT0g",
+    authDomain: "chatroom-99c68.firebaseapp.com",
+    projectId: "chatroom-99c68",
+    storageBucket: "chatroom-99c68.appspot.com",
+    messagingSenderId: "334106384398",
+    appId: "1:334106384398:web:e31847632236e6fc81445f"
+};
+
+initializeApp(firebaseConfig)
