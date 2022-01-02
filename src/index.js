9da8e112683acabbd4ca410ee928c7ff258cf2ff
Adriana <am20dipi@alum.siena.edu>
2022-01-02 15:53:03 -0500

    Building out template for chatroom in index.html; adding script tags and link tags to css and bootstrap; starting to initialize firebase app and services using firestore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 //console.log("hello")
 
 import { initializeApp } from 'firebase/app'
+import { 
+    getFirestore, collection
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyAu6iwGs95JYD9xTt1ZJJ5HIbw-P9LLT0g",
@@ -11,4 +14,17 @@ const firebaseConfig = {
     appId: "1:334106384398:web:e31847632236e6fc81445f"
 };
 
+// initialize firebase app
 initializeApp(firebaseConfig)
+
+
+// initialize services
+const db = getFirestore()
+    // db === database
+
+
+// collection reference
+const collectionRef = collection(db, '')
+
+
+// get collection data
